bd8b11adc03cc87cd3d7f1b2e0f68f9c171f8d01
Ashutosh Kaushal <ashutoshkl1412@gmail.com>
2026-03-10 12:35:02 +0530

    new about and home layout

--- content.js
+++ content.js
@@ -6,17 +6,19 @@ const CONFIG = {
     // Google Apps Script URL yahan dalein
     scriptURL: "https://script.google.com/macros/s/AKfycbwi9kCj0ALebjHcpNacSzAaRkF2ePkzcq9r5BlIIjO-y9Rbr6aXVH45aFD5Rvh2CqlX/exec",
 
-    mentor: { 
-        name: "Shiwendu Kaushal", 
-        role: "FOUNDER & CHIEF EDUCATOR", 
-        ig: "https://www.instagram.com/shiwendu.kaushal" 
+    mentor: {
+        name: "Shiwendu Kaushal",
+        role: "Co-Founder & CEO",
+        ig: "https://www.instagram.com/shiwendu.kaushal",
+        pic : "photo link/path"
     },
     developer: {
         name: "Ashutosh Kaushal",
-        role: "FOUNDER & LEAD ARCHITECT",
+        role: "Co-Founder & Managing Director(MD)",
         bio: "The technical visionary behind Xplainer Guru. Managing platform infrastructure and digital strategy.",
         ig: "https://instagram.com/ashu7061a",
-        li: "https://linkedin.com/in/ashutoshkaushal-1412"
+        li: "https://linkedin.com/in/ashutoshkaushal-1412",
+        pic: "photo link/path"
     }
 };
 
